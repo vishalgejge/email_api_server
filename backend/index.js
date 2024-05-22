7be8bfd00d0580bab6d4a50b9264042fffbd94b2
Vishal Gejge <92996047+vishalgejge@users.noreply.github.com>
2024-05-22 22:42:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const env = require('dotenv')
+const env = require('dotenv');
 const cors = require('cors');
 const pdfRoute = require('./pdfRoutes');
-const path = require('path')
 
-env.config()
+env.config();
 
-const port = 8001;
+// Set up CORS middleware
+app.use(cors({
+    origin: "https://email-api-server-front.vercel.app", // Allow requests from this origin
+    methods: ["POST", "GET"], // Allow these HTTP methods
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}));
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
 
-app.use(pdfRoute)
+// Define your API routes
+app.use('/api/pdf', pdfRoute);
 
-app.listen(port,()=>{
-    console.log(`Server running on ${port}`);
-})
+// Start the server
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
