a62db79c9f00ced635bdbeeb6794f01f85a96477
Vishal Gejge <92996047+vishalgejge@users.noreply.github.com>
2024-05-22 23:50:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/api', pdfRoute);
+app.use('/api', emailRoute);
 // Start the server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
